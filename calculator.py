1c14e1d6573209ceeacc4a5ca3c76262fb08b2ff
danaellis <danarellis@gmail.com>
2019-01-09 13:13:05 -0500

    add square, cube, square_n_times functions to calculator.py

--- calculator.py
+++ calculator.py
@@ -6,6 +6,12 @@ def subtract(a, b):
 	return a - b
 def divide(a, b):
 	return a/b
+def square(a):
+	return a ** 2
+def cube(a):
+	return a ** 3
+def square_n_times(number, n):
+	return number ** (2*n) 
 
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
